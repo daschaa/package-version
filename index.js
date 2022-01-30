584a0ad30f12e3abeee7b3e38b7294b9ed7b9540
Joshua Weber <josh@joshuaw.de>
2022-01-30 21:29:45 +0100

    :sparkles: Uses execution path to get package.json path

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import core from "@actions/core";
-import github from "@actions/github";
 import fs from "fs";
-
 try {
     const path = core.getInput('path');
-    let packageFile = JSON.parse(fs.readFileSync(path));
+    const cleanedPath = `${process.cwd()}${path.startsWith('./') ? path.slice(1, path.length) : '/' + path}`;
+    let packageFile = JSON.parse(fs.readFileSync(cleanedPath));
     core.setOutput("version", packageFile.version);
 } catch (e) {
     core.setFailed(e);
